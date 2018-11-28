04dc05ec848657dbe3ee5936beeb0873a7751b12
Norbert Korczyk <korczyk.norbert@gmail.com>
2018-11-28 21:17:42 +0100

    Lesson-5 done

--- script.js
+++ script.js
@@ -4,29 +4,25 @@ class App extends React.Component {
     age: 30
   };
 
-  handleClick = (event) => {
+  handleChange = (event) => {
     this.setState({
-      name: 'yoshi',
-      age: 25
+      name: event.target.value
     });
-    console.log(this.state);
   }
 
-  handleMouseOver = (event) => {
-    console.log(event.target, event.pageX);
-  }
-
-  handleCopy = (event) => {
-    console.log("Try being original");
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('Form submitted', this.state.name);
   }
 
   render() {
     return (<div className="app-content">
       <h1>Hello ninjas</h1>
-      <p>My name is {this.state.name} and I am  {this.state.age}.</p>
-      <button onClick={this.handleClick}>Click me</button>
-      <button onMouseOver={this.handleMouseOver}>Hover Me</button>
-      <p onCopy={this.handleCopy}>What we think, we became</p>
+      <p>My name is {this.state.name}.</p>
+      <form onSubmit={this.handleSubmit}>
+        <input type="text" onChange={this.handleChange} />
+        <button>Submit</button>
+      </form>
     </div>)
   }
 }
