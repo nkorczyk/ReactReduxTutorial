bcfe82f7cbc2901f5207a15d21a5b6878f62a6d8
Norbert Korczyk <korczyk.norbert@gmail.com>
2018-11-28 20:35:18 +0100

    Lesson-2 done

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 class App extends React.Component {
+  state = {
+    name: 'Ryu',
+    age: 30
+  };
+
   render() {
     return (<div className="app-content">
-        <h1>Hello ninjas</h1>
-        <p>{Math.random() * 10}</p>
-      </div>)
+      <h1>Hello ninjas</h1>
+      <p>My name is {this.state.name} and I am  {this.state.age}.</p>
+    </div>)
   }
 }
 
