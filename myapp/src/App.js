edce526569fa2ca3d6ca4439e711ec312a2b800e
Norbert Korczyk <korczyk.norbert@gmail.com>
2018-11-28 21:33:53 +0100

    Lesson-6 done

---
+++ myapp/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>My first react app</h1>
+        <p>Welcome</p>
+      </div>
+    );
+  }
+}
+
+export default App;
