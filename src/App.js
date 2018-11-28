c420e0a8d9a7e3b0f2078bc0f666c22206d5a183
Norbert Korczyk <korczyk.norbert@gmail.com>
2018-11-28 21:58:09 +0100

    Lesson-8 done

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
       <div className="App">
         <h1>My first react app</h1>
         <p>Welcome</p>
-        <Ninjas />
+        <Ninjas name="Ryu" age="25" belt="black"/>
       </div>
     );
   }
