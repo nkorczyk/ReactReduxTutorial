78290c54a9bd207971d5ea5414bfd77732b3d5f8
Norbert Korczyk <korczyk.norbert@gmail.com>
2018-11-28 20:55:30 +0100

    Lesson-3 done

--- script.js
+++ script.js
@@ -4,10 +4,25 @@ class App extends React.Component {
     age: 30
   };
 
+  handleClick(event) {
+    console.log(event.target);
+  }
+
+  handleMouseOver(event) {
+    console.log(event.target, event.pageX);
+  }
+
+  handleCopy(event) {
+    console.log("Try being original");
+  }
+
   render() {
     return (<div className="app-content">
       <h1>Hello ninjas</h1>
       <p>My name is {this.state.name} and I am  {this.state.age}.</p>
+      <button onClick={this.handleClick}>Click me</button>
+      <button onMouseOver={this.handleMouseOver}>Hover Me</button>
+      <p onCopy={this.handleCopy}>What we think, we became</p>
     </div>)
   }
 }
