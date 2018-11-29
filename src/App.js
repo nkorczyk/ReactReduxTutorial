d13f7bf8e759cab3216b5c016419244483d42df2
Norbert Korczyk <korczyk.norbert@gmail.com>
2018-11-29 09:56:34 +0100

    Lesson-12 done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import Ninjas from './Ninjas';
+import AddNinja from './AddNinja';
 
 class App extends Component {
   state = {
     ninjas: [
-      {name: 'Ryu', age: 30, belt: 'black', id: 1},
-      {name: 'Yoshi', age: 20, belt: 'green', id: 2},
-      {name: 'Crystal', age: 25, belt: 'pink', id: 3}
+      { name: 'Ryu', age: 30, belt: 'black', id: 1 },
+      { name: 'Yoshi', age: 20, belt: 'green', id: 2 },
+      { name: 'Crystal', age: 25, belt: 'pink', id: 3 }
     ]
   };
 
@@ -15,7 +16,8 @@ class App extends Component {
       <div className="App">
         <h1>My first react app</h1>
         <p>Welcome</p>
-        <Ninjas ninjas={this.state.ninjas}/>
+        <Ninjas ninjas={this.state.ninjas} />
+        <AddNinja />
       </div>
     );
   }
