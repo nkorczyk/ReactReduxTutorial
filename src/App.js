4c855fbdc82e9c1667fa7872df9a86c124e432cd
Norbert Korczyk <korczyk.norbert@gmail.com>
2018-11-29 23:09:10 +0100

    TODO app - part 1

--- src/App.js
+++ src/App.js
@@ -1,54 +1,29 @@
 import React, { Component } from 'react';
-import Ninjas from './Ninjas';
-import AddNinja from './AddNinja';
+import Todos from './Todos';
 
 class App extends Component {
   state = {
-    ninjas: [
-      { name: 'Ryu', age: 30, belt: 'black', id: 1 },
-      { name: 'Yoshi', age: 20, belt: 'green', id: 2 },
-      { name: 'Crystal', age: 25, belt: 'pink', id: 3 }
+    todos: [
+      { id: 1, content: 'buy some milk' },
+      { id: 2, content: 'play mario cart' }
     ]
   };
 
-  addNinja = (ninja) => {
-    ninja.id = Math.random();
-    let ninjas = [...this.state.ninjas, ninja];
-
-    this.setState({
-      ninjas: ninjas
-    });
-  }
-
-  deleteNinja = (id) => {
-    let ninjas = this.state.ninjas.filter(ninja => {
-      return ninja.id !== id;
+  deleteTodo = (id) => {
+    const todos = this.state.todos.filter(todo => {
+      return todo.id !== id;
     });
 
     this.setState({
-      ninjas: ninjas
+      todos
     });
-  }
-
-  // Life cycle hooks
-  // fires when the component first mounts to the DOM
-  componentDidMount() {
-    console.log("component mounted");
-  }
-
-  // fires when we get a change of states or props
-  componentDidUpdate(prevProps, prevState) {
-    console.log("component updated");
-    console.log(prevProps, prevState);
-  }
+  };
 
   render() {
     return (
-      <div className="App">
-        <h1>My first react app</h1>
-        <p>Welcome</p>
-        <Ninjas deleteNinja={this.deleteNinja} ninjas={this.state.ninjas} />
-        <AddNinja addNinja={this.addNinja}/>
+      <div className="App container">
+        <h1 className="center blue-text">Todo's</h1>
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
       </div>
     );
   }
