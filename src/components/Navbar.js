8c7316023d6eb603fdc22934360429ee3ac7f8cb
Norbert Korczyk <korczyk.norbert@gmail.com>
2018-11-30 13:27:01 +0100

    React Router - part 2

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,17 @@
 import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <nav className="nav-wrapper red darken-3">
       <a className="brand-logo">Poke'Times</a>
       <ul className="right">
-        <li><a href="/">Home</a></li>
-        <li><a href="/about">About</a></li>
-        <li><a href="/contact">Contact</a></li>
+        <li><Link to="/">Home</Link></li>
+        <li><NavLink to="/about">About</NavLink></li>
+        <li><NavLink to="/contact">Contact</NavLink></li>
       </ul>
     </nav>
   )
 }
 
 export default Navbar;
- 
