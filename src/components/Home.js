1ddb36a9edeb48f0209729536dc3b9396ef72301
Norbert Korczyk <korczyk.norbert@gmail.com>
2018-12-31 14:05:45 +0100

    Mapping State to Props

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,32 +1,32 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 import { Link } from 'react-router-dom';
 import Pokeball from '../pokeball.png';
+import { connect } from 'react-redux';
 
 class Home extends Component {
-  state = {
-    posts: []
-  };
+  // state = {
+  //   posts: []
+  // };
 
   // Good place to fetch some data from the server
-  componentDidMount() {
-
-    axios.get('https://jsonplaceholder.typicode.com/posts')
-      .then(response => {
-        // console.log(response);
-        this.setState({
-          posts: response.data.slice(0, 10)
-        })
-      });
-  }
+  // componentDidMount() {
+  //   axios.get('https://jsonplaceholder.typicode.com/posts')
+  //     .then(response => {
+  //       // console.log(response);
+  //       this.setState({
+  //         posts: response.data.slice(0, 10)
+  //       })
+  //     });
+  // }
 
   render() {
-    const { posts } = this.state;
+    console.log(this.props);
+    const { posts } = this.props;
     const postList = posts.length ? (
       posts.map(post => {
         return (
           <div className="post card" key={post.id}>
-          <img src={Pokeball} alt="A pokeball"/>
+            <img src={Pokeball} alt="A pokeball" />
             <div className="card-content">
               <Link to={'/' + post.id}>
                 <span className="card-title red-text">{post.title}</span>
@@ -49,4 +49,10 @@ class Home extends Component {
   }
 }
 
-export default Home;
+const mapStateToProps = (state) => {
+  return {
+    posts: state.posts
+  }
+};
+
+export default connect(mapStateToProps)(Home);
