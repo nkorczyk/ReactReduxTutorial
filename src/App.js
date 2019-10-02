7cbe327a42a4c7af144ef34758c6690b66e037b9
Norbert Korczyk <korczyk.norbert@gmail.com>
2019-10-02 23:42:13 +0200

    Refactoring

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
-import Navbar from "./components/Navbar";
 import { BrowserRouter, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
 import Contact from "./components/Contact";
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,28 @@
-import React from 'react';
-import { Link, NavLink, withRouter } from 'react-router-dom';
+import React from "react";
+import { Link, NavLink, withRouter } from "react-router-dom";
 
-const Navbar = (props) => {
+const Navbar = props => {
   // setTimeout(() => {
   //   props.history.push('/about');
   // }, 2000);
 
   return (
     <nav className="nav-wrapper red darken-3">
+      {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
       <a className="brand-logo">Poke'Times</a>
       <ul className="right">
-        <li><Link to="/">Home</Link></li>
-        <li><NavLink to="/about">About</NavLink></li>
-        <li><NavLink to="/contact">Contact</NavLink></li>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <NavLink to="/about">About</NavLink>
+        </li>
+        <li>
+          <NavLink to="/contact">Contact</NavLink>
+        </li>
       </ul>
     </nav>
-  )
-}
+  );
+};
 
 export default withRouter(Navbar);
